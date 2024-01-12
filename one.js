33bec67fc0b7bfae98ba61637dfe75bf959eaa2d
Likksi <153167133+Likksi@users.noreply.github.com>
2024-01-12 13:43:13 +0300

    Update one.js

--- one.js
+++ one.js
@@ -1,10 +1,10 @@
-const result = +prompt('Введите четное число', '')
-if (typeof +result !== 'number') {
+const result = +prompt('Введите четное число')
+if (isNaN(result)) {
   alert('Упс, кажется, вы ошиблись')
-} else if (isNaN(result % 2)) {
-  alert('Ошибка, введено НЕ ЧИСЛО')
-} else if (result % 2 === 0) {
-  alert('Число четное')
 } else {
-  alert('число нечетное')
+  if (result % 2 === 0) {
+    alert('Число четное')
+  } else {
+    alert('число нечетное')
+  }
 }
